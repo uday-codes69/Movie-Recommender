f29688354efa3e4d42c4d1022f60f558c20abab6
Uday Thakur <uday35700@gmail.com>
2025-08-14 01:59:05 +0530

    Refactor app.py to remove similarity.pkl and compute similarity on the fly

--- app.py
+++ app.py
@@ -4,21 +4,21 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Load local data (movies.pkl should have 'title' and 'tags')
+# Load movies data (movies.pkl should have 'title' and 'tags')
 movies_dict = pickle.load(open('movies.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 
-# Compute similarity on the fly
+# Compute similarity matrix on the fly
 cv = CountVectorizer(max_features=5000, stop_words='english')
 vectors = cv.fit_transform(movies['tags']).toarray()
 similarity = cosine_similarity(vectors)
 
-# Recommend function
+# Recommend movies function
 def recommend(movie):
     if movie not in movies['title'].values:
         return []
     movie_index = movies[movies['title'] == movie].index[0]
-    distances = similarity[movie_index]
+    distances = similarity[movie_index].flatten()
     movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
     return [movies.iloc[i[0]].title for i in movie_list]
 
@@ -30,7 +30,7 @@ movie_name = st.selectbox("Choose a movie", movies['title'].values)
 if st.button("Recommend"):
     recs = recommend(movie_name)
     if recs:
-        st.write("### Recommended Movies:")
+        st.subheader("Recommended Movies:")
         for m in recs:
             st.write(m)
     else:
